855566bd112d5dcd78a0ff7d1fd06a7d23fabe4b
Babbaj <babbaj45@gmail.com>
2019-04-22 23:17:19 -0400

    problem-7

---
+++ problem-7/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <array>
+#include <cmath>
+
+bool isPrime(const int n) {
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+
+    int num = 0;
+    for (int i = 2; ; i++) {
+        if (isPrime(i)) {
+            num++;
+        }
+        if (num >= 10001) {
+            std::cout << i << '\n';
+            break;
+        }
+    }
+
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
